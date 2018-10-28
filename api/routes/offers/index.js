114003ff195fd238beaee58d7d5a19aa0c0bf8bc
franps <franps.94@hotmail.com>
2018-10-28 16:18:21 -0300

    added offers routes and basic handlers, dummy funcs

---
+++ api/routes/offers/index.js
@@ -0,0 +1,11 @@
+const handlers = require('./handlers');
+const validators = require('./validators');
+
+//TO DO add validators and correct parameters
+module.exports = router => {
+  router.post('/offers/:id1/:id2', handlers.create);
+  router.get('/offers/', handlers.find);
+  router.get('/offers/:id', handlers.userOffers);
+
+  return router;
+};
