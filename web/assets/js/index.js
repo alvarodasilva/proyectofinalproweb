f8b4fc351b4a6427b5363b3f1889baeb48ef8df6
guillenoble <guillermo.noble@hotmail.com>
2018-10-28 22:52:24 -0300

    Change main article card load function to make it work with pre loaded articles

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -1,65 +1,121 @@
-const articles = [];
+/* fetch("http://localhost:8000/users").then(res=>(articles = res:)) */
 
-function getArticleData() {
+const articles = [
+  {
+    name: null,
+    type: null,
+    description: null,
+    picture: null,
+    /*user_id: null,
+    user_name: null,*/
+  },
+  {
+    name: null,
+    type: null,
+    description: null,
+    picture: null,
+    /*user_id: null,
+    user_name: null,*/
+  },
+  {
+    name: null,
+    type: null,
+    description: null,
+    picture: null,
+    /*user_id: null,
+    user_name: null,*/
+  },
+];
+
+function showArticles(articlesTemp) {
+  let html = "<div class='wrapper' id='articleCard'>";
+  for (let i = 0; i < articlesTemp.length; i += 1) {
+    html += "<div class='product-img'>";
+    html +=
+      "<img id='articleImg' height='320' width='327' src='" +
+      articlesTemp[i].picture +
+      "' />";
+    html += '</div>';
+    html += "<div class='product-info'>";
+    html += "<div class='product-text'>";
+    html += "<h1 id='articleName'>" + articlesTemp[i].name + '</h1>';
+    html += '<h2>@Guillenoble</h2>';
+    html +=
+      "<h3 class='articleType' id='articleType'>" +
+      articlesTemp[i].type +
+      '</h3>';
+    html +=
+      "<p id='articleDescription'>" + articlesTemp[i].description + '</p>';
+    html += '<h2>Like this product to trade</h2>';
+    html +=
+      "<input id='likeBoxButton' type='image' src='/images/heart(24).png'>";
+    html += '</div>';
+    html += '</div>';
+  }
+  html += '</div>';
+  const demo = document.getElementById('demo');
+  demo.innerHTML = html;
+}
+
+showArticles(articles);
+
+function createArticleCard() {
   const articleNameInput = document.getElementById('articleNameInput').value;
   const articleDescriptionInput = document.getElementById(
     'articleDescriptionInput',
   ).value;
   const articleTypeInput = document.getElementById('articleTypeInput').value;
   const file = document.getElementById('file').value;
-  const newProduct = {
-    articleName: null,
-    articleType: null,
-    articleDescription: null,
-    articlePicture: null,
+  const newArticle = {
+    name: null,
+    type: null,
+    description: null,
+    picture: null,
   };
-  console.log(file);
   if (
-    articleDescriptionInput.length <= 60 &&
+    articleDescriptionInput.length <= 100 &&
     articleDescriptionInput.length >= 10
   ) {
-    newProduct.articleName = articleNameInput;
-    newProduct.articleDescription = articleDescriptionInput;
-    newProduct.articleType = articleTypeInput;
-    newProduct.articlePicture = file;
-    articles.push(newProduct);
-    console.log(articles);
+    newArticle.name = articleNameInput;
+    newArticle.description = articleDescriptionInput;
+    newArticle.type = articleTypeInput;
+    newArticle.picture = file;
+    articles.push(newArticle);
   } else {
-    alert('Article description is too short');
+    alert('Article description is too short or long');
   }
+  showArticles(articles);
+}
+/*
+
+function hacerAlgo() {
   let html = "<div class='wrapper' id='articleCard'>";
   for (let i = 0; i < articles.length; i += 1) {
-    /*  <div class="product-img">
-                <img id="articleImg" height="320" width="327">
-            </div> */
     html += "<div class='product-img'>";
     html +=
       "<img id='articleImg' height='320' width='327' src='" +
-      articles[i].articlePicture +
+      articles[i].picture +
       "' />";
     html += '</div>';
     html += "<div class='product-info'>";
     html += "<div class='product-text'>";
-    html += "<h1 id='articleName'>" + articles[i].articleName + '</h1>';
+    html += "<h1 id='articleName'>" + articles[i].name + '</h1>';
     html += '<h2>@Guillenoble</h2>';
     html +=
-      "<h3 class='articleType' id='articleType'>" +
-      articles[i].articleType +
-      '</h3>';
-    html +=
-      "<p id='articleDescription'>" + articles[i].articleDescription + '</p>';
+      "<h3 class='articleType' id='articleType'>" + articles[i].type + '</h3>';
+    html += "<p id='articleDescription'>" + articles[i].description + '</p>';
     html += '<h2>Like this product to trade</h2>';
-    /* html += "<img src='heart(24).png'>"; */
     html +=
       "<input id='likeBoxButton' type='image' src='/images/heart(24).png'>";
     html += '</div>';
     html += '</div>';
   }
-
   html += '</div>';
-  document.getElementById('demo').innerHTML = html;
+  const demo = document.getElementById('demo');
+  demo.innerHTML = html;
 }
 
+*/
 document.getElementById('addArticle').addEventListener('click', () => {
-  getArticleData();
+  createArticleCard();
 });
