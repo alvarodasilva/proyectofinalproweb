2a2fab4ffac0b6c1fe7632745636d444459423ec
guillenoble <guillermo.noble@hotmail.com>
2018-11-01 16:22:52 -0300

    Add box to offer a product

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -6,70 +6,75 @@ const articles = [
     type: null,
     description: null,
     picture: null,
-    /* user_id: null,
-    user_name: null, */
   },
   {
     name: null,
     type: null,
     description: null,
     picture: null,
-    /* user_id: null,
-    user_name: null, */
   },
   {
     name: null,
     type: null,
     description: null,
     picture: null,
-    /* user_id: null,
-    user_name: null, */
   },
 ];
 
-/* var str = `Hello, ${name}!`; */
+const offerArticleBox = document.getElementsByClassName('offerArticleBox');
+/* Opens box to offer article */
+function prueba() {
+  if (offerArticleBox.style.display === 'none') {
+    offerArticleBox.style.display = 'block';
+  } else {
+    offerArticleBox.style.display = 'none';
+  }
+}
 
+/* Shows article card */
 function showArticles(articlesTemp) {
-  let html = "<div class='wrapper' id='articleCard'>";
+  console.log('show articles');
+  let html = "<div class='wrapper'>";
   for (let i = 0; i < articlesTemp.length; i += 1) {
     html += "<div class='product-img'>";
-    html += `<img id='articleImg' height='320' width='327' src=${
-      articlesTemp[i].picture
-    }/>`;
+    html += `<img height='320' width='327' src='${articlesTemp[i].picture}'/>`;
     html += '</div>';
     html += "<div class='product-info'>";
     html += "<div class='product-text'>";
-    html += `<h1 id='articleName'> ${articlesTemp[i].name} </h1>`;
+    html += `<h1> ${articlesTemp[i].name} </h1>`;
     html += '<h2>@Guillenoble</h2>';
-    html += `<h3 class='articleType' id='articleType'> ${
-      articlesTemp[i].type
-    } </h3>`;
-    html += `<p id='articleDescription'> ${articlesTemp[i].description} </p>`;
+    html += `<h3 class='articleType'> ${articlesTemp[i].type} </h3>`;
+    html += `<p> ${articlesTemp[i].description} </p>`;
     html += '<h2>Like this product to trade</h2>';
     html +=
-      "<input id='likeBoxButton' type='image' src='/images/heart(24).png'>";
+      "<input type='image' src='/images/heart(24).png' onclick='prueba()'>";
     html += '</div>';
     html += '</div>';
   }
   html += '</div>';
   const showArticleCard = document.getElementById('demo');
   showArticleCard.innerHTML = html;
+  console.log('show articles 2');
 }
 
 showArticles(articles);
 
+/* Creates article card */
 function createArticleCard() {
   const articleNameInput = document.getElementById('articleNameInput').value;
   const articleDescriptionInput = document.getElementById(
     'articleDescriptionInput',
   ).value;
   const articleTypeInput = document.getElementById('articleTypeInput').value;
-  const file = document.getElementById('file').value;
+  const file = document.getElementById('file').files[0];
+
   const newArticle = {
     name: null,
     type: null,
     description: null,
     picture: null,
+    user_id: null,
+    user_name: null,
   };
   if (
     articleDescriptionInput.length <= 100 &&
@@ -85,36 +90,7 @@ function createArticleCard() {
   }
   showArticles(articles);
 }
-/*
 
-function hacerAlgo() {
-  let html = "<div class='wrapper' id='articleCard'>";
-  for (let i = 0; i < articles.length; i += 1) {
-    html += "<div class='product-img'>";
-    html +=
-      "<img id='articleImg' height='320' width='327' src='" +
-      articles[i].picture +
-      "' />";
-    html += '</div>';
-    html += "<div class='product-info'>";
-    html += "<div class='product-text'>";
-    html += "<h1 id='articleName'>" + articles[i].name + '</h1>';
-    html += '<h2>@Guillenoble</h2>';
-    html +=
-      "<h3 class='articleType' id='articleType'>" + articles[i].type + '</h3>';
-    html += "<p id='articleDescription'>" + articles[i].description + '</p>';
-    html += '<h2>Like this product to trade</h2>';
-    html +=
-      "<input id='likeBoxButton' type='image' src='/images/heart(24).png'>";
-    html += '</div>';
-    html += '</div>';
-  }
-  html += '</div>';
-  const demo = document.getElementById('demo');
-  demo.innerHTML = html;
-}
-
-*/
 document.getElementById('addArticle').addEventListener('click', () => {
   createArticleCard();
 });
