71fba5e65f95d6467de2101f8b81c5198ffd452f
federicomoyamartin <federicomoyamartin@gmail.com>
2018-11-06 19:17:34 -0300

    Add field status for the Offer model

--- api/models/offer.js
+++ api/models/offer.js
@@ -7,9 +7,12 @@ const offerSchema = new mongoose.Schema(
     bidder_article_id: { type: String, required: true },
     user_id: { type: String, required: true },
     article_id: { type: String, required: true },
+    status: { type: String, required: true },
   },
   { timestamps: true },
 );
+
+/* Possible values for status??? */
 
 /* Create the model from the schema. */
 const Offer = mongoose.model('Offer', offerSchema);
