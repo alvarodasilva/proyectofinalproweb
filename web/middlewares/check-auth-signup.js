e3a11f8c24748f692f16bb80c3292ebd15d2867f
federicomoyamartin <federicomoyamartin@gmail.com>
2018-11-20 21:01:57 -0300

    HOT FIX

--- web/middlewares/check-auth-signup.js
+++ web/middlewares/check-auth-signup.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
       req.cookies.access_token !== undefined &&
       req.cookies.access_token !== null
     ) {
-      const token = req.cookies.access_token.split(' ')[1];
+      const token = req.cookies.access_token.split('Bearer')[1];
       const decoded = jwt.verify(token, process.env.JWT_KEY);
       req.userData = decoded;
       res.redirect('/');
